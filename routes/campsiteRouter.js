2457ca50c5170c2549124504296c1e5347610fc5
Dan Garro <dobetterwithdan@gmail.com>
2022-12-10 10:29:24 -0500

    task 1 untested

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get('/:campsiteId', (req, res) => {
+    res.end(`Will send campsite with id: ${res.body.id}`);
+})
+.post('/:campsiteId', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`); 
+})
+.put('/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites'); 
+})
+.delete('/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite with id: ${req.body.id}`); 
+});
+
 module.exports = campsiteRouter;
